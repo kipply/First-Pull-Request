abae33e9ad04f6044905a6f4a56e512671c0047e
Alexander Maximovitch <sasham+git@maximdim.com>
2016-10-27 16:08:45 -0400

    Added Class
    added class to the program

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
-public static main void (String[] args) {
-  //do something
-  System.out.println("Hello World");
-}
+class Main{
+  
+  public static main void (String[] args) {
+    //do something
+    System.out.println("Hello World");
+  }
+}// end Main class
