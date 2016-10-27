3dcb343eab4c1fd125b937543697a7e7be2d78dc
Mark Zhang <markzhang.inbox@gmail.com>
2016-10-27 18:38:32 -0400

    Added variable in java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public static main void (String[] args) {
   List<String> al = new ArrayList<String>();
+  String coolVar = "Stuff";
+  System.out.println(coolVar);
   //cool comments that is here
   //do something
   //cool comment
