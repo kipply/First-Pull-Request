deb34689c9067750db0af59d55be9427d49dedd2
Alexander Maximovitch <sasham+git@maximdim.com>
2016-10-27 16:11:42 -0400

    Added Variable
    #35

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ class Main{
   public static main void (String[] args) {
     //do something
     System.out.println("Hello World");
+    
+    Int num = 5;
+    System.out.println(num);
   }
 }// end Main class
