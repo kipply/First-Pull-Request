a6a26ff976c6d67226ba679d934d609dd4f15b1d
Tania Nag <tanianag998@gmail.com>
2018-10-13 00:48:10 +0530

    next line

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ class Main{
   
   public static main void (String[] args) {
     //do something
-    System.out.println("Hello World");
-    
+    System.out.print("Hello World");
+     System.out.print("\n");
     // This is a variable!
     int num = 5;
     System.out.println(num);
